f7ceaebf2330a84fdad9161c8eed46afaa295933
Evan Daniel <evandaniel@Evans-MacBook-Pro.local>
2020-08-26 13:08:24 -0500

    2020_08_26 sample and logic to get a single char.

--- 20200824_Files.c
+++ 20200824_Files.c
@@ -3,6 +3,24 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <string.h>
+#include <termios.h>
+
+// Custom getchar()-type function to get a single character without the user having to press enter/return.  
+// It requires the <termios.h> header.  
+// the function gets the information about the terminal and stores it in the variable "old", then creates a copy, "new."
+// It then turns off two flags (the "ICANON" AND "ECHO" flags) using bit operators, and sets the terminal to use the new copy.  
+// After the text is entered, the terminal is set back to its old version.  
+char getcharsingle() {
+    struct termios old, new; 
+    tcgetattr(0, &old); 
+    new = old; 
+    new.c_lflag &= ~ICANON; 
+    new.c_lflag &= ~ECHO; 
+    tcsetattr(0, 0, &new); 
+    char c = getchar(); 
+    tcsetattr(0, 0, &old); 
+    return c; 
+}
 
 int main(int argc, char* argv[argc+1]) {
 
@@ -26,15 +44,29 @@ int main(int argc, char* argv[argc+1]) {
         return EXIT_FAILURE; 
     }
 
+    // When a user enters a character, print out the appropriate range of lines from the text.  
     // Print one line of text for each row in the terminal except the last (to prevent scrolling once the program exits).  
     // We use a buffer (think of it as an array for us to temporarily hold data); it has to have room at the end for the terminating '\0' at the end of a string (AKA array of chars).  
     // "fgets()" stores the current line (up to a given length, here "ws.ws_col", or the number of columns in our terminal) of the file into the buffer, and then moves to the next line.  
-    for(int y = 0; y < ws.ws_row - 1; y++) {
+    char user_input; 
+    long int position = 0; 
+    do {
+        fseek(txt_file, position, SEEK_SET); 
         char buffer[ws.ws_col + 1]; 
-        fgets(buffer, ws.ws_col, txt_file); 
-        printf("%s", buffer); 
-    }
-
+        if(user_input == 's') {
+            fgets(buffer, ws.ws_col, txt_file); 
+            position = ftell(txt_file); 
+        } else if(user_input == 'w') {
+            // ???
+            // Write some logic that will allow the user the travel to earlier lines in the text.  
+        }
+        for(int y = 0; y < ws.ws_row - 1; y++) {
+            fgets(buffer, ws.ws_col, txt_file); 
+            printf("%s", buffer); 
+        }
+        printf("\n"); 
+    } while((user_input = getcharsingle()) != 'q'); 
+    
     // No failure.  
     fclose(txt_file); 
     printf("\n"); 
