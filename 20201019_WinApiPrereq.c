3e68a445663ae8185252dac389c3c98b10ed7cb4
evan <evandaniel@sckans.edu>
2020-10-23 10:19:42 -0500

    win32api

---
+++ 20201019_WinApiPrereq.c
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    MessageBox(NULL, "Hello, nice world!", "Whatever We want", MB_OK); 
+    return 0; 
+}
